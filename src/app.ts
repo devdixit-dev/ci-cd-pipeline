32a17e6c5f7c90e0d50b690019c979aedbda278b
devdixit-dev <devdixitsocial@gmail.com>
2026-04-29 21:30:18 +0530

    add: implement home route to return a welcome message

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,12 @@ import express from "express";
 
 export const app = express();
 
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: "Home page"
+  })
+})
+
 app.get("/health", (_req, res) => {
   res.status(200).json({
     status: "ok"
